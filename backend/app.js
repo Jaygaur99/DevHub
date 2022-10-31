99777098a23457cb56692f0098f7283a1a9f9f48
Jay Kumar Gaur <jaygaur99@gmail.com>
2022-10-31 20:26:05 +0530

    Added Middleware packages in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const httpsServer = require("http").createServer(app);
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
+const compression = require("compression");
+const httpsServer = require("http").createServer(app);
 
+// Adding middleware
+app.use(morgan("tiny"));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 const corsOption = {
   origin: process.env.CLIENT_URL,
   credentials: true,
 };
 app.use(cors(corsOption));
+app.use(helmet());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use(compression());
+
+// Adding Routes
+
+// Socket.io configuration
 
 exports.app = app;
 exports.httpsServer = httpsServer;
