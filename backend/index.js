a83ba6418a79e487357b539c3cd4e7f98dde573c
Jay Kumar Gaur <jaygaur99@gmail.com>
2022-10-31 19:03:47 +0530

    Added mongoDB configuration Stopped tracking .env file

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 require("dotenv").config();
 const { app, httpsServer } = require("./app");
+const connectDB = require("./Config/database");
+const cloudinary = require("cloudinary").v2;
+
+connectDB();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const { PORT } = process.env;
 
