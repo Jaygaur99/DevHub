8ba8e372e603f07f00d5fb095b4b18a670ff7994
Jay Kumar Gaur <jaygaur99@gmail.com>
2022-11-05 13:55:02 +0530

    Included run on local network command

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const { app, httpsServer } = require('./app');
-const connectDB = require('./Config/database');
-const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+const { app, httpsServer } = require("./app");
+const connectDB = require("./Config/database");
+const cloudinary = require("cloudinary").v2;
 
 connectDB();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 const { PORT } = process.env;
 
 httpsServer.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
